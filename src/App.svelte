1b363a47a122f441cba891583b88f16abc5774c3
Karzan Botani <karzan@botani.nu>
2022-09-20 09:47:30 +0200

    feat: fetch Chuck Norris categories and populate dropdown

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import Dropdown from './lib/Dropdown.svelte';
+
+  let value: string;
+  let categories: Array<string> = [];
+
+  onMount(async () => {
+    void fetchCategories();
+  });
+
+  async function fetchCategories() {
+    const url = 'https://api.chucknorris.io/jokes/categories';
+
+    try {
+      const response = await fetch(url);
+      categories = await response.json();
+      value = categories[0];
+    } catch (e) {
+      console.error('Error fetching categories:', e);
+    }
+  }
 </script>
 
-<main />
+<main>
+  <div>
+    <Dropdown {categories} {value} />
+  </div>
+</main>
 
 <style>
 </style>
---
+++ src/lib/Dropdown.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let value: string;
+  export let categories: Array<string>;
+</script>
+
+<label for="categories">Select a category:</label>
+<select name="categories" id="categories" bind:value>
+  {#each categories as category}
+    <option value={category}>{category}</option>
+  {/each}
+</select>
