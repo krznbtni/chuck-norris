6cc282bb57457fc0a28107e571f50a755f18eae4
Karzan Botani <karzan@botani.nu>
2022-09-20 14:19:37 +0200

    feat: display modal with random joke

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import Dropdown from './lib/Dropdown.svelte';
+  import Modal from './lib/Modal.svelte';
+  import chuckNorrisLogo from './assets/chuck-norris.png';
+
+  interface Joke {
+    categories: Array<string>;
+    created_at: string;
+    icon_url: string;
+    id: string;
+    updated_at: string;
+    url: string;
+    value: string;
+  }
 
   let selectedCategory: string;
   let categories: Array<string> = [];
+  let joke: Joke;
+  let modal: Modal;
 
   onMount(async () => {
     void fetchCategories();
@@ -20,13 +34,46 @@
       console.error('Error fetching categories:', e);
     }
   }
+
+  async function fetchJoke() {
+    const url = `https://api.chucknorris.io/jokes/random?category=${selectedCategory}`;
+
+    try {
+      const response = await fetch(url);
+      joke = await response.json();
+      modal.open();
+    } catch (e) {
+      console.error('Error fetching joke:', e);
+    }
+  }
 </script>
 
 <main>
-  <div>
+  <div class="dropdown">
     <Dropdown {categories} bind:value={selectedCategory} />
   </div>
+
+  <Modal bind:this={modal}>
+    <button on:click={fetchJoke} slot="trigger" type="button">
+      Joke It Up
+    </button>
+
+    <div slot="content">
+      <img class="img" src={chuckNorrisLogo} alt="Chuck Norris" />
+      <p>{joke.value}</p>
+    </div>
+  </Modal>
 </main>
 
 <style>
+  .dropdown {
+    margin-bottom: 20px;
+  }
+
+  .img {
+    background-color: yellow;
+    width: 100px;
+    border-radius: 50%;
+    border: 2px solid black;
+  }
 </style>
